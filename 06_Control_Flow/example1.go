600a40828ed1fb32a87b61f8e325cf14f73e1355
Rexford Nyarko <rexfordnyrk@gmail.com>
2017-10-02 07:39:52 +0000

    added comments to control flow example 1 code

--- 06_Control_Flow/example1.go
+++ 06_Control_Flow/example1.go
@@ -4,6 +4,8 @@ import "fmt"
 
 func main(){
 
+	/*Write a program that prints out all the numbers between 1 and 100 that are
+	evenly divisible by 3 (i.e., 3, 6, 9, etc.).*/
 	for i:= 1; i < 100; i++ {
 		if i % 3 == 0{
 			fmt.Printf("%v \n", i)
